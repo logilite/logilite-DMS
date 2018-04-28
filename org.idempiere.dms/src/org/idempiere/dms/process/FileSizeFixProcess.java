3f25e30928cffe954ecd9ad769413c4cbae5e045
pdudhat <pdudhat@logilite.com>
2018-04-28 11:38:57 +0530

    refector file size rename fix process

--- org.idempiere.dms/src/org/idempiere/dms/process/FileSizeFixProcess.java
+++ org.idempiere.dms/src/org/idempiere/dms/process/FileSizeFixProcess.java
@@ -59,13 +59,23 @@ public class FileSizeFixProcess extends SvrProcess
 	@Override
 	protected String doIt() throws Exception
 	{
-		int cntMigrated = 0;
-		
+		int cntMigratedwithoutVersion = 0;
+		int cntMigratedwithVersion = 0;
 		addLog("Process started..");
+		addLog("MIGRATING WITHOUT VERSION");
+		cntMigratedwithoutVersion = migrateWithoutVersion();
+		//addLog("MIGRATING WITH VERSION");
+		//cntMigratedwithVersion = migrateWithVersion();
+		addLog("Process complete withoutVersion(" + cntMigratedwithoutVersion + ") withVersion ("+cntMigratedwithVersion+") file renamed..");
+		return "Process complete withoutVersion(" + cntMigratedwithoutVersion + ") withVersion ("+cntMigratedwithVersion+") file renamed..";
+	}
 
+	private int migrateWithoutVersion()
+	{
+		int cntMigrated = 0;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		HashMap<I_DMS_Content, I_DMS_Association> contentList = getDMSContents();
+		HashMap<I_DMS_Content, I_DMS_Association> contentList = getDMSContents(false);
 		for (Map.Entry<I_DMS_Content, I_DMS_Association> entry : contentList.entrySet())
 		{
 
@@ -73,26 +83,25 @@ public class FileSizeFixProcess extends SvrProcess
 			MDMSAssociation mdmsAssociation = (MDMSAssociation) entry.getValue();
 
 			I_DMS_MimeType mimytype = mdmsContent.getDMS_MimeType();
-			if (mimytype.getFileExtension() == ".def" || mdmsContent.getName().endsWith(mimytype.getFileExtension()))
+			if (mimytype.getFileExtension().equalsIgnoreCase(".def"))
 			{
 				continue;
 			}
-
+			
+			if (mdmsContent.getName().endsWith(mimytype.getFileExtension()))
+			{
+				continue;
+			}
+			
 			String oldname = mdmsContent.getName().replaceAll("\\s+$", "");
 			String newName = oldname + mimytype.getFileExtension();
-			addLog("start rename : [" + oldname +"] to [" + newName +"] dms_content_id[" + mdmsContent.getDMS_Content_ID() + "]" );
-			renameFile(mdmsContent, mdmsAssociation, newName);
-			addLog("rename DONE : [" + oldname +"] to [" + newName +"] dms_content_id[" + mdmsContent.getDMS_Content_ID() + "]" );
-			cntMigrated++;
 
 			int DMS_Content_ID = mdmsContent.getDMS_Content_ID();
-			MDMSContent content = null;
-			MDMSAssociation association = null;
 			try
 			{
 				String sql = "SELECT a.DMS_Content_ID, a.DMS_Association_ID "
 						+ "		FROM dms_association a INNER JOIN dms_content c ON a.dms_content_id = c.dms_content_id "
-						+ "		WHERE AND a.dms_associationtype_id = 1000000 AND DMS_Content_Related_ID = ? "
+						+ "		WHERE a.dms_associationtype_id = 1000000 AND DMS_Content_Related_ID = ? "
 						+ "		Order By a.DMS_Association_ID";
 				
 				pstmt = DB.prepareStatement(sql, null);
@@ -100,22 +109,18 @@ public class FileSizeFixProcess extends SvrProcess
 
 				rs = pstmt.executeQuery();
 
-				while (rs.next())
+				if (!rs.next())
 				{
-					content = new MDMSContent(Env.getCtx(), rs.getInt("DMS_Content_ID"), null);
-					association = new MDMSAssociation(Env.getCtx(), rs.getInt("DMS_Association_ID"), null);
-					String newVersionName = oldname + "("+association.getSeqNo()+")" +mimytype.getFileExtension();
-					addLog("start rename version : [" + content.getName() +"] to [" + newVersionName +"] dms_content_id[" + content.getDMS_Content_ID() + "]" );
-					renameFile(content, association, newVersionName);
-					addLog("rename version DONE: [" + content.getName() +"] to [" + newVersionName +"] dms_content_id[" + content.getDMS_Content_ID() + "]" );
-					cntMigrated++;
+					if(renameFile(mdmsContent, mdmsAssociation, newName)){
+						cntMigrated++;
+					}
 				}
 			}
 			catch (Exception e)
 			{
 				log.log(Level.SEVERE, "Rename content failure.", e);
-				int contentID = content != null ? content.getDMS_Content_ID() : 0 ;
-				addLog("Fail to rename version oldName[" + content.getName() +"] dms_content_id[" + contentID + "]" );
+				int contentID = mdmsContent != null ? mdmsContent.getDMS_Content_ID() : 0 ;
+				addLog("Fail to rename version oldName[" + mdmsContent.getName() +"] dms_content_id[" + contentID + "]" );
 				throw new AdempiereException("Rename content failure: " + e.getLocalizedMessage());
 			}
 			finally
@@ -126,21 +131,29 @@ public class FileSizeFixProcess extends SvrProcess
 			}
 
 		}
-		addLog("Process complete sucessfully..");
-		return "Process Completed. (" + cntMigrated + ") file renamed..";
+		return cntMigrated;
 	}
 
-	private HashMap<I_DMS_Content, I_DMS_Association> getDMSContents()
+	private HashMap<I_DMS_Content, I_DMS_Association> getDMSContents(boolean version)
 	{
 		HashMap<I_DMS_Content, I_DMS_Association> map = new LinkedHashMap<I_DMS_Content, I_DMS_Association>();
 
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
+		String sql = null;
 		try
 		{
-			String sql = "SELECT a.DMS_Content_ID, a.DMS_Association_ID "
-					+ "     FROM dms_association a INNER JOIN dms_content c ON a.dms_content_id = c.dms_content_id "
-					+ "     WHERE c.contentbasetype = ? AND length(c.name) >= 60 AND a.dms_associationtype_id <> 1000000";
+			if(!version){
+				sql = "SELECT a.DMS_Content_ID, a.DMS_Association_ID "
+						+ "     FROM dms_association a INNER JOIN dms_content c ON a.dms_content_id = c.dms_content_id "
+						+ "     WHERE c.contentbasetype = ? AND length(c.name) = 60 AND a.dms_associationtype_id <> 1000000";
+			}else{
+				sql = "SELECT DMS_Content_ID, DMS_Association_ID FROM dms_association "
+						+ " WHERE dms_content_id IN (SELECT a.DMS_Content_Related_ID FROM dms_content c "
+						+ " JOIN dms_association a ON c.dms_content_id = a.dms_content_id " 
+						+ " WHERE c.contentbasetype = ? and length(c.name) = 60 AND a.dms_associationtype_id = 1000000 )";
+			}
+			
 			
 			pstmt = DB.prepareStatement(sql, null);
 			pstmt.setString(1, X_DMS_Content.CONTENTBASETYPE_Content);
@@ -171,10 +184,12 @@ public class FileSizeFixProcess extends SvrProcess
 		return map;
 	}
 
-	private void renameFile(MDMSContent content, MDMSAssociation association, String newName)
+	private boolean renameFile(MDMSContent content, MDMSAssociation association, String newName)
 	{
+		boolean flag = false;
 		if (fileStorgProvider.getFile(contentManager.getPath(content)) != null)
 		{
+			addLog(content.getDMS_Content_ID() + "| old name ["+content.getName()+"] to new name ["+newName+"] |parentPath ["+content.getParentURL()+"] | SUCCESS");
 			String newPath = fileStorgProvider.getFile(contentManager.getPath(content)).getAbsolutePath();
 			// String fileExt = newPath.substring(newPath.lastIndexOf("."),
 			// newPath.length());
@@ -188,27 +203,108 @@ public class FileSizeFixProcess extends SvrProcess
 
 			content.setName(newFile.getAbsolutePath().substring(newFile.getAbsolutePath().lastIndexOf(spFileSeprator) + 1,
 					newFile.getAbsolutePath().length()));
+			
+			//addLog("File available  : dms_content_id[" + content.getDMS_Content_ID() + "]");
+			content.saveEx();
+			try
+			{
+				Map<String, Object> solrValue = Utils.createIndexMap(content, association);
+				indexSeracher.deleteIndex(content.getDMS_Content_ID());
+				indexSeracher.indexContent(solrValue);
+			}
+			catch (Exception e)
+			{
+				log.log(Level.SEVERE, "RE-Indexing of Content Failure :", e);
+				addLog("RE-Indexing of Content Failure : dms_content_id[" + content.getDMS_Content_ID() + "]");
+				throw new AdempiereException("RE-Indexing of Content Failure :" + e);
+			}
+			flag =  true;
+			
 		}
 		else
 		{
-			addLog("NO File available  : dms_content_id[" + content.getDMS_Content_ID() + "]");
-			content.setName(newName);
+			addLog(content.getDMS_Content_ID() + "| old name ["+content.getName()+"] to new name ["+newName+"] |parentPath ["+content.getParentURL()+"] | FAIL");
+			//addLog("NO File available  : dms_content_id[" + content.getDMS_Content_ID() + "]");
+			//content.setName(newName);
 		}
+		return flag;
+	}
+	
+	private int migrateWithVersion()
+	{
 
-		content.saveEx();
+		int cntMigrated = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		HashMap<I_DMS_Content, I_DMS_Association> contentList = getDMSContents(true);
+		for (Map.Entry<I_DMS_Content, I_DMS_Association> entry : contentList.entrySet())
+		{
 
-		try
-		{
-			Map<String, Object> solrValue = Utils.createIndexMap(content, association);
-			indexSeracher.deleteIndex(content.getDMS_Content_ID());
-			indexSeracher.indexContent(solrValue);
+			MDMSContent mdmsContent = (MDMSContent) entry.getKey();
+			MDMSAssociation mdmsAssociation = (MDMSAssociation) entry.getValue();
+
+			I_DMS_MimeType mimytype = mdmsContent.getDMS_MimeType();
+			if (mimytype.getFileExtension().equalsIgnoreCase(".def"))
+			{
+				continue;
+			}
+			
+			if (mdmsContent.getName().endsWith(mimytype.getFileExtension()))
+			{
+				continue;
+			}
+			
+			String oldname = mdmsContent.getName().replaceAll("\\s+$", "");
+			String newName = oldname + mimytype.getFileExtension();
+		//	addLog("start rename : [" + oldname +"] to [" + newName +"] dms_content_id[" + mdmsContent.getDMS_Content_ID() + "]" );
+			renameFile(mdmsContent, mdmsAssociation, newName);
+		//	addLog("rename DONE : [" + oldname +"] to [" + newName +"] dms_content_id[" + mdmsContent.getDMS_Content_ID() + "]" );
+			cntMigrated++;
+
+			int DMS_Content_ID = mdmsContent.getDMS_Content_ID();
+			MDMSContent content = null;
+			MDMSAssociation association = null;
+			try
+			{
+				String sql = "SELECT a.DMS_Content_ID, a.DMS_Association_ID "
+						+ "		FROM dms_association a INNER JOIN dms_content c ON a.dms_content_id = c.dms_content_id "
+						+ "		WHERE a.dms_associationtype_id = 1000000 AND DMS_Content_Related_ID = ? "
+						+ "		Order By a.DMS_Association_ID";
+				
+				pstmt = DB.prepareStatement(sql, null);
+				pstmt.setInt(1, DMS_Content_ID);
+
+				rs = pstmt.executeQuery();
+
+				while (rs.next())
+				{
+					content = new MDMSContent(Env.getCtx(), rs.getInt("DMS_Content_ID"), null);
+					association = new MDMSAssociation(Env.getCtx(), rs.getInt("DMS_Association_ID"), null);
+					String newVersionName = oldname + "("+association.getSeqNo()+")" +mimytype.getFileExtension();
+				//	addLog("start rename version : [" + content.getName() +"] to [" + newVersionName +"] dms_content_id[" + content.getDMS_Content_ID() + "]" );
+					renameFile(content, association, newVersionName);
+				//	addLog("rename version DONE: [" + content.getName() +"] to [" + newVersionName +"] dms_content_id[" + content.getDMS_Content_ID() + "]" );
+					cntMigrated++;
+				}
+			}
+			catch (Exception e)
+			{
+				log.log(Level.SEVERE, "Rename content failure.", e);
+				int contentID = content != null ? content.getDMS_Content_ID() : 0 ;
+				addLog("Fail to rename version oldName[" + content.getName() +"] dms_content_id[" + contentID + "]" );
+				throw new AdempiereException("Rename content failure: " + e.getLocalizedMessage());
+			}
+			finally
+			{
+				DB.close(rs, pstmt);
+				rs = null;
+				pstmt = null;
+			}
+
 		}
-		catch (Exception e)
-		{
-			log.log(Level.SEVERE, "RE-Indexing of Content Failure :", e);
-			addLog("RE-Indexing of Content Failure : dms_content_id[" + content.getDMS_Content_ID() + "]");
-			throw new AdempiereException("RE-Indexing of Content Failure :" + e);
-		}
+		return cntMigrated;
+	
+		
 	}
 
 }
