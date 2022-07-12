771946cdd5ccd3b1030f2e90125f048ff991fddc
sbhimani <sbhimani@logilite.com>
2022-07-12 22:02:24 +0530

    Fixed query issue

--- com.logilite.dms.uuid/src/com/logilite/dms/uuid/contant/DMSContantUUID.java
+++ com.logilite.dms.uuid/src/com/logilite/dms/uuid/contant/DMSContantUUID.java
@@ -13,16 +13,18 @@ public class DMSContantUUID
 	public static final String	SQL_OLD_NEW_PATH	= " WITH RECURSIVE SupplyTree(AD_Client_ID, DMS_Content_ID, DMS_Content_Related_ID, DMS_Association_ID) AS 							"
 														+ " ( 																															"
 														+ " 	SELECT 	c.AD_Client_ID, c.DMS_Content_ID, a.DMS_Content_Related_ID, a.DMS_Association_ID, a.AD_Table_ID, a.Record_ID,	"
-														+ " 			c.Value, c.Name, c.ParentURL, 1 AS LEVEL, ARRAY['', c.Value] AS Path, a.DMS_AssociationType_ID 					"
+														+ " 			v.Value, c.Name, c.ParentURL, 1 AS LEVEL, ARRAY['', v.Value] AS Path, a.DMS_AssociationType_ID 					"
 														+ " 	FROM DMS_content c 																										"
 														+ " 	INNER JOIN DMS_association a 	ON (a.DMS_Content_ID = c.DMS_Content_ID) 												"
+														+ "		INNER JOIN DMS_Version v	 	ON (v.DMS_Content_ID = c.DMS_Content_ID AND v.SeqNo = 0)								"
 														+ " 	WHERE ParentURL IS NULL AND c.AD_Client_ID = ? 																			"
 														+ " UNION ALL 																													"
 														+ " 	SELECT	c.AD_Client_ID, c.DMS_Content_ID, a.DMS_Content_Related_ID, a.DMS_Association_ID, a.AD_Table_ID, a.Record_ID,	"
-														+ " 			c.Value, c.Name, c.ParentURL,  LEVEL + 1, Path || c.Value, a.DMS_AssociationType_ID 							"
+														+ " 			v.Value, c.Name, c.ParentURL,  LEVEL + 1, Path || v.Value, a.DMS_AssociationType_ID 							"
 														+ " 	FROM  DMS_Association a 																								"
 														+ " 	INNER JOIN supplytree st 		ON (st.DMS_Content_ID = a.DMS_Content_Related_ID AND a.AD_Client_ID = ?) 				"
 														+ " 	INNER JOIN DMS_content c 		ON (c.DMS_Content_ID = a.DMS_Content_ID) 												"
+														+ "		INNER JOIN DMS_Version v	 	ON (v.DMS_Content_ID = c.DMS_Content_ID AND v.SeqNo = 0)								"
 														+ " 	WHERE a.DMS_AssociationType_ID IS NULL OR a.DMS_AssociationType_ID = 1000001											"
 														+ " )																															"
 														+ " SELECT 																														"
@@ -38,7 +40,7 @@ public class DMSContantUUID
 														+ " WHERE (c.IsMounting = 'Y' AND Level > 2) OR (c.IsMounting = 'N')															"
 														+ " ORDER BY Level DESC, s.AD_Client_ID, s.ParentURL, Path, s.DMS_Content_ID, s.AD_Table_ID, s.Record_ID, s.DMS_Content_Related_ID";
 
-	public static final String	SQL_COUNT_VERSION	= "SELECT COUNT(1) FROM DMS_Version v "
+	public static final String	SQL_COUNT_VERSION	= " SELECT COUNT(1) FROM DMS_Version v "
 														+ " INNER JOIN DMS_content c ON (c.DMS_content_ID = v.DMS_content_ID)"
 														+ " WHERE v.AD_Client_ID = ? AND c.IsMounting = 'N' ";
 
