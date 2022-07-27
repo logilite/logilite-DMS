0731b5d432ae931bf34e126d076deeafe46d222f
sbhimani <sbhimani@logilite.com>
2022-07-27 23:26:47 +0530

    Implemented thread to command file execute directly to shell console.

--- com.logilite.dms.uuid/src/com/logilite/dms/uuid/process/ConvertRelationalToRelationalUUID.java
+++ com.logilite.dms.uuid/src/com/logilite/dms/uuid/process/ConvertRelationalToRelationalUUID.java
@@ -1,8 +1,11 @@
 package com.logilite.dms.uuid.process;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,6 +14,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 
 import org.adempiere.exceptions.AdempiereException;
@@ -26,6 +32,7 @@ import org.idempiere.dms.constant.DMSConstant;
 
 import com.logilite.dms.uuid.classes.UUIDContentManager;
 import com.logilite.dms.uuid.contant.DMSContantUUID;
+import com.logilite.dms.uuid.util.UtilsUUID;
 
 /**
  * Process for converting relational DMS data structure to relational UUID structure
@@ -52,12 +59,18 @@ import com.logilite.dms.uuid.contant.DMSContantUUID;
 public class ConvertRelationalToRelationalUUID extends SvrProcess
 {
 
-	private boolean	p_ClientInfoSetContentTypeToUUID	= false;
-	private boolean	p_ExportCmdFileForChangeToUUID		= false;
-	private boolean	p_ExportWithBaseDirPath				= false;
-	private boolean	p_IsModifyContentParentURL			= false;
+	private boolean			p_ClientInfoSetContentTypeToUUID	= false;
+	private boolean			p_ExportCmdFileForChangeToUUID		= false;
+	private boolean			p_ExportWithBaseDirPath				= true;
+	private boolean			p_IsModifyContentParentURL			= false;
+	private boolean			p_IsExecuteShellCommandDirectly		= false;
 
-	private int		p_NoOfRecordsExportPerFile			= 100000;
+	private int				p_NoOfRecordsExportPerFile			= 100000;
+	private int				p_NoOfThreadInPool					= 1;
+
+	private ArrayList<File>	fileList							= new ArrayList<File>();
+	private ArrayList<File>	fileListOutput						= new ArrayList<File>();
+	private CountDownLatch	latch;
 
 	@Override
 	protected void prepare()
@@ -74,6 +87,10 @@ public class ConvertRelationalToRelationalUUID extends SvrProcess
 				p_ExportWithBaseDirPath = para[i].getParameterAsBoolean();
 			else if ("NoOfRecordsExportPerFile".equals(name))
 				p_NoOfRecordsExportPerFile = para[i].getParameterAsInt();
+			else if ("IsExecuteShellCommandDirectly".equals(name))
+				p_IsExecuteShellCommandDirectly = para[i].getParameterAsBoolean();
+			else if ("NoOfThreadInPool".equals(name))
+				p_NoOfThreadInPool = para[i].getParameterAsInt();
 			else if ("IsModifyContentParentURL".equals(name))
 				p_IsModifyContentParentURL = para[i].getParameterAsBoolean();
 			else if ("ClientInfoSetContentTypeToUUID".equals(name))
@@ -93,6 +110,8 @@ public class ConvertRelationalToRelationalUUID extends SvrProcess
 		 */
 		if (p_ExportCmdFileForChangeToUUID)
 		{
+			String filePrefix = "DMS_UUID_" + DMSConstant.SDF_NO_SPACE.format(new Date());
+
 			DMS dms = new DMS(getAD_Client_ID());
 			String baseDir = (p_ExportWithBaseDirPath ? dms.getFileStorageProvider().getBaseDirectory(null) : "");
 
@@ -101,6 +120,8 @@ public class ConvertRelationalToRelationalUUID extends SvrProcess
 			ResultSet rs = null;
 			try
 			{
+				statusUpdate("Preparing data for exporting command files each table wise + No of records limit");
+
 				String key = "";
 				int count = 0;
 				int noOfRecords = DB.getSQLValue(get_TrxName(), DMSContantUUID.SQL_COUNT_VERSION, getAD_Client_ID());
@@ -112,6 +133,9 @@ public class ConvertRelationalToRelationalUUID extends SvrProcess
 				pstmt.setString(4, DMSConstant.FILE_SEPARATOR);
 				pstmt.setString(5, DMSConstant.FILE_SEPARATOR);
 				rs = pstmt.executeQuery();
+
+				statusUpdate("Going to prepare command list file ");
+
 				while (rs.next())
 				{
 					int tableID = rs.getInt("AD_Table_ID");
@@ -135,7 +159,7 @@ public class ConvertRelationalToRelationalUUID extends SvrProcess
 					count++;
 
 					if (count % 1000 == 0)
-						statusUpdate("UUID based path created " + count + " out of " + noOfRecords);
+						statusUpdate("UUID based path created " + count + " out of " + noOfRecords + " ( Approx )");
 				}
 			}
 			catch (SQLException e)
@@ -150,8 +174,52 @@ public class ConvertRelationalToRelationalUUID extends SvrProcess
 				pstmt = null;
 			}
 
+			File file = new File(System.getProperty("java.io.tmpdir") + filePrefix);
+			if (!file.exists())
+			{
+				file.mkdir();
+			}
+
+			// Write data to the file
+			writeToFile(filePrefix, mapCMDList);
+
+			// Script file download
+			downloadableZip(filePrefix, file);
+
 			//
-			writeToFile(mapCMDList);
+			if (p_IsExecuteShellCommandDirectly)
+			{
+				String fileOutPrefix = filePrefix + "_Out";
+				File fileOutputDir = new File(System.getProperty("java.io.tmpdir") + fileOutPrefix);
+				if (!fileOutputDir.exists())
+				{
+					fileOutputDir.mkdir();
+				}
+
+				//
+				latch = new CountDownLatch(fileList.size());
+				ExecutorService executor = Executors.newFixedThreadPool(p_NoOfThreadInPool);
+
+				for (File file1 : fileList)
+				{
+					Runnable worker = new CommandExeThread(file1, fileOutPrefix, this);
+					executor.execute(worker);
+				}
+
+				try
+				{
+					latch.await();
+				}
+				catch (InterruptedException e)
+				{
+					log.saveError("Error- ", e.getMessage());
+					throw new AdempiereException(e);
+				}
+				executor.shutdown();
+
+				//
+				downloadableZip(fileOutPrefix, fileOutputDir);
+			}
 		}
 
 		/**
@@ -160,13 +228,16 @@ public class ConvertRelationalToRelationalUUID extends SvrProcess
 		 */
 		if (p_IsModifyContentParentURL)
 		{
+			statusUpdate("Updating the content parent url");
 
-			int no = DB.executeUpdateEx(DMSContantUUID.SQL_MODIFY_CONTENT_PARENTURL, new Object[] {
-																									getAD_Client_ID(),
-																										DMSConstant.FILE_SEPARATOR,
-																										getAD_Client_ID(),
-																										DMSConstant.FILE_SEPARATOR,
-																										DMSConstant.FILE_SEPARATOR }, get_TrxName());
+			int no = DB.executeUpdateEx(DMSContantUUID.SQL_MODIFY_CONTENT_PARENTURL,
+										new Object[] {
+														getAD_Client_ID(),
+															DMSConstant.FILE_SEPARATOR,
+															getAD_Client_ID(),
+															DMSConstant.FILE_SEPARATOR,
+															DMSConstant.FILE_SEPARATOR
+										}, get_TrxName());
 
 			addLog("#" + no + " content parent URL updated.");
 			log.log(Level.INFO, "#" + no + " content parent URL updated.");
@@ -188,22 +259,21 @@ public class ConvertRelationalToRelationalUUID extends SvrProcess
 		return "";
 	} // doIt
 
-	void writeToFile(HashMap<String, ArrayList<String>> mapCMDList)
+	void writeToFile(String filePrefix, HashMap<String, ArrayList<String>> mapCMDList)
 	{
 		int seqNo = 10;
 		int count = 0;
 		int resetTableID = -1;
-		String filePrefix = "DMS_UUID_" + DMSConstant.SDF_NO_SPACE.format(new Date());
 
 		ArrayList<String> masterListPerFile = new ArrayList<String>();
 		boolean requireToSplitFile = false;
 		Object[] keys = mapCMDList.keySet().toArray();
 		Arrays.sort(keys);
+
 		for (Object objKey : keys)
 		{
 			String key = (String) objKey;
 			int tableID = Integer.parseInt(key.split("_")[0]);
-			// int recordID = Integer.parseInt(key.split("_")[1]);
 			if (resetTableID != -1 && resetTableID != tableID || requireToSplitFile)
 			{
 				createFile(resetTableID, masterListPerFile, seqNo, count, filePrefix);
@@ -222,7 +292,6 @@ public class ConvertRelationalToRelationalUUID extends SvrProcess
 			ArrayList<String> cmdList = mapCMDList.get(key);
 			masterListPerFile.addAll(cmdList);
 			count += cmdList.size();
-			System.out.println(key + "  " + cmdList.size());
 
 			if (count / p_NoOfRecordsExportPerFile >= 1)
 			{
@@ -242,11 +311,11 @@ public class ConvertRelationalToRelationalUUID extends SvrProcess
 		if (tableID > 0)
 			tablename = MTable.get(getCtx(), tableID).getTableName();
 
-		String fileName = filePrefix + "_" + seqNo + "_" + tablename + (Env.isWindows() ? ".txt" : ".sh");
+		String fileName = filePrefix + "_" + seqNo + "_" + tablename + (Env.isWindows() ? ".bat" : ".sh");
 		File fileDownload = null;
 		try
 		{
-			fileDownload = new File(System.getProperty("java.io.tmpdir") + File.separator + fileName);
+			fileDownload = new File(System.getProperty("java.io.tmpdir") + File.separator + filePrefix + File.separator + fileName);
 			if (!fileDownload.createNewFile())
 			{
 				addLog("Failed to create temporary file with name:" + fileName);
@@ -264,9 +333,135 @@ public class ConvertRelationalToRelationalUUID extends SvrProcess
 
 		addLog("File created: " + fileName + ", with " + masterListPerFile.size() + " command lines");
 
-		//
-		processUI.download(fileDownload);
-
+		fileList.add(fileDownload);
 	} // createFile
 
+	private void downloadableZip(String filePrefix, File file) throws Exception
+	{
+		String headDirName = filePrefix + "_";
+		File srcFolder = file;
+		if (!srcFolder.exists())
+		{
+			srcFolder.mkdir();
+		}
+
+		File destZipFile = null;
+		try
+		{
+			destZipFile = File.createTempFile(headDirName, ".zip");
+			destZipFile.createNewFile();
+		}
+		catch (Throwable e)
+		{
+			throw new AdempiereException("Unable to create temp file", e);
+		}
+		destZipFile.delete();
+
+		UtilsUUID.zipFolder(Paths.get(srcFolder.getAbsolutePath()), Paths.get(destZipFile.getAbsolutePath()));
+
+		processUI.download(destZipFile);
+		srcFolder.deleteOnExit();
+	} // downloadableZip
+
+	/**
+	 * to concurrent execution shell command
+	 * 
+	 * @author Sachin Bhimani
+	 */
+	class CommandExeThread implements Runnable
+	{
+		File								file;
+		String								filePrefix;
+		ConvertRelationalToRelationalUUID	crtruProcess;
+
+		public CommandExeThread(File file, String filePrefix, ConvertRelationalToRelationalUUID process)
+		{
+			this.file = file;
+			this.filePrefix = filePrefix;
+			this.crtruProcess = process;
+		}
+
+		@Override
+		public void run()
+		{
+			StringBuilder output = new StringBuilder();
+			try
+			{
+				output.append("File: ").append(file.getAbsolutePath()).append("\n\n");
+
+				ProcessBuilder processBuilder = new ProcessBuilder();
+
+				// -- Linux --
+
+				// Run a shell command
+				// processBuilder.command("bash", "-c", "ls /home/SB/");
+
+				// Run a shell script
+				// processBuilder.command("path/to/hello.sh");
+
+				// -- Windows --
+				// Run a command
+				// processBuilder.command("cmd.exe", "/c", "dir C:\\Users\\SB");
+
+				// Run a bat file
+				// processBuilder.command("C:\\Users\\SB\\hello.bat");
+
+				processBuilder.command(file.getAbsolutePath());
+				Process process = processBuilder.start();
+
+				BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+				String line;
+				while ((line = reader.readLine()) != null)
+				{
+					output.append(line + "\n");
+				}
+
+				int exitVal = process.waitFor();
+				if (exitVal == 0)
+				{
+					output.append("\nSuccess! \n");
+				}
+				else
+				{
+					output.append("\nAbnormal! \n");
+				}
+			}
+			catch (IOException e)
+			{
+				output.append("\n\n\nIOException:\n");
+				output.append(e.getLocalizedMessage() + "\n\n" + e);
+			}
+			catch (InterruptedException e)
+			{
+				output.append("\n\n\nInterruptedException:\n");
+				output.append(e.getLocalizedMessage() + "\n\n" + e);
+			}
+			finally
+			{
+				crtruProcess.latch.countDown();
+			}
+
+			// Output file create
+			try
+			{
+				String fileName = file.getName() + "_" + Thread.currentThread().getId() + ".out";
+				File outFile = new File(System.getProperty("java.io.tmpdir") + File.separator + filePrefix + File.separator + fileName);
+				if (!outFile.createNewFile())
+				{
+					addLog("Failed to create temporary file with name:" + fileName);
+				}
+
+				// append a list of lines, add new lines automatically
+				Files.writeString(outFile.toPath(), output, StandardOpenOption.APPEND);
+
+				//
+				fileListOutput.add(outFile);
+			}
+			catch (IOException e)
+			{
+				log.log(Level.SEVERE, "Failed to write output file of the shell command execution result. error:" + e.getLocalizedMessage());
+			}
+		} // run
+	} // Class CommandExeThread
 }
--- com.logilite.dms.uuid/src/com/logilite/dms/uuid/util/UtilsUUID.java
+++ com.logilite.dms.uuid/src/com/logilite/dms/uuid/util/UtilsUUID.java
@@ -1,7 +1,16 @@
 package com.logilite.dms.uuid.util;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.compiere.model.MTable;
@@ -53,4 +62,20 @@ public class UtilsUUID
 		version.saveEx();
 		return version;
 	} // createVersionUU
+
+	// Create a Zip from the given path
+	public static void zipFolder(Path sourceFolderPath, Path zipPath) throws Exception
+	{
+		ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipPath.toFile()));
+		Files.walkFileTree(sourceFolderPath, new SimpleFileVisitor<Path>() {
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
+			{
+				zos.putNextEntry(new ZipEntry(sourceFolderPath.relativize(file).toString()));
+				Files.copy(file, zos);
+				zos.closeEntry();
+				return FileVisitResult.CONTINUE;
+			}
+		});
+		zos.close();
+	} // zipFolder
 }
