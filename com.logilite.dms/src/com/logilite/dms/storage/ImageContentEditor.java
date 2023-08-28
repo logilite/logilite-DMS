b3f73f8e3a4da9c95fba0b5188459d33b3134453
bpatel <bpatel@logilite.com>
2023-08-28 12:23:56 +0530

    Implement ZoomOut  And ZoomIn feature for the Image

--- com.logilite.dms/src/com/logilite/dms/storage/ImageContentEditor.java
+++ com.logilite.dms/src/com/logilite/dms/storage/ImageContentEditor.java
@@ -14,33 +14,43 @@
 package com.logilite.dms.storage;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Base64;
 
+import org.adempiere.base.Core;
 import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.webui.component.Panel;
+import org.apache.commons.io.FileUtils;
 import org.compiere.model.I_AD_StorageProvider;
 import org.compiere.util.CLogger;
-import org.compiere.util.Util;
-import org.zkoss.util.media.AMedia;
-import org.zkoss.zul.Iframe;
+import org.zkoss.io.Files;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Div;
+import org.zkoss.zul.Script;
 
 import com.logilite.dms.factories.IContentEditor;
 import com.logilite.dms.model.I_DMS_Content;
 
-public class ImageContentEditor extends Panel implements IContentEditor
+public class ImageContentEditor extends Panel implements IContentEditor, EventListener<Event>
 {
 
 	/**
 	 * 
 	 */
-	private static final long	serialVersionUID	= 7241323248511502223L;
+	private static final long		serialVersionUID			= 7241323248511502223L;
+	private static final String		EVENT_ON_LOAD_DMS_CONTENT	= "onLoadDMSContent";
 
-	public static CLogger		log					= CLogger.getCLogger(ImageContentEditor.class);
+	public static CLogger			log							= CLogger.getCLogger(ImageContentEditor.class);
 
-	File						file				= null;
-	I_DMS_Content				content				= null;
-	I_AD_StorageProvider		provider;
-	String						baseDir;
+	private Div						divzoomcontent				= new Div();
+	private File					file						= null;
+	private String					src;
+	@SuppressWarnings("unused")
+	private I_DMS_Content			content						= null;
 
 	@Override
 	public void setFile(File file)
@@ -57,45 +67,71 @@ public class ImageContentEditor extends Panel implements IContentEditor
 	@Override
 	public void init(I_AD_StorageProvider storageProvider)
 	{
-		provider = storageProvider;
-		baseDir = storageProvider.getFolder();
+		
 	}
 
 	@Override
 	public Panel initPanel()
 	{
-		this.setHeight("100%");
-		this.setWidth("100%");
-		this.setStyle("overflow: auto; -webkit-overflow-scrolling: touch; width: 100%;");
+		loadPanAndZoomJS();
 
-		AMedia media = null;
+		getImageSrc();
 
-		try
-		{
-			String fileName = content.getName();
-			String format = content.getDMS_MimeType().getFileExtension();
-			if (!Util.isEmpty(format, true) && !fileName.endsWith(format))
-			{
-				media = new AMedia(fileName.concat(format), null, null, file, null);
-			}
-			else
-			{
-				media = new AMedia(file, null, null);
-			}
-		}
-		catch (FileNotFoundException e)
-		{
-			throw new AdempiereException("Document cannot be displayed:" + e.getLocalizedMessage());
-		}
+		this.appendChild(divzoomcontent);
 
-		// TODO Need to image fit in preview
-		Iframe iframeContentPriview = new Iframe();
-		iframeContentPriview.setSclass("dms-image-frame-viewer");
-		iframeContentPriview.setContent(media);
-		iframeContentPriview.setWidth("100%");
-		iframeContentPriview.setHeight("100%");
-		this.appendChild(iframeContentPriview);
+		divzoomcontent.addEventListener(EVENT_ON_LOAD_DMS_CONTENT, this);
+		
+		Events.echoEvent(EVENT_ON_LOAD_DMS_CONTENT, divzoomcontent, null);
 
 		return this;
 	} // initPanel
+
+	/**
+	 * Get JS of PanAndZoom And Append it
+	 */
+	public void loadPanAndZoomJS()
+	{
+		// Load JS of PanAndZoom On the DMS Content
+		Script script = new Script();
+		URL urlJS = Core.getResourceFinder().getResource("js/dmscontentpreview_min.js");
+		if (urlJS == null)
+			throw new AdempiereException("Fail to load scanner JS");
+
+		try
+		{
+			byte[] byteCode = Files.readAll(urlJS.openStream());
+			script.setContent(new String(byteCode));
+			divzoomcontent.appendChild(script);
+		}
+		catch (IOException e)
+		{
+			throw new AdempiereException("Fail to load script : " + e.getLocalizedMessage(), e);
+		}
+	}
+
+	/**
+	 * Get Base64 encoder src of the Image File
+	 */
+	public void getImageSrc()
+	{
+		try
+		{
+			byte[] bytes = FileUtils.readFileToByteArray(file);
+			src = Base64.getEncoder().encodeToString(bytes);
+		}
+		catch (IOException e)
+		{
+			throw new AdempiereException("Fail to encode src of the image file: " + e.getLocalizedMessage(), e);
+		}
+	}
+
+	@Override
+	public void onEvent(Event event) throws Exception
+	{
+		if (EVENT_ON_LOAD_DMS_CONTENT.equalsIgnoreCase(event.getName()))
+		{
+			// Create canvas as per encoder src of image in the Image Container
+			Clients.evalJavaScript("onPenAndZoom('" + this.getUuid() + "', '" + src + "')");
+		}
+	}
 }
