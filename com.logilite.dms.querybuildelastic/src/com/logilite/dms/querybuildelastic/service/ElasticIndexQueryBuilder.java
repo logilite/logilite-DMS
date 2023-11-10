22e9681a4472b242c86824abe5427d8dcd453b82
Sachin Bhimani <sbhimani@logilite.com>
2023-11-10 12:36:42 +0530

    Query Formation changes as per Elastic works

--- com.logilite.dms.querybuildelastic/src/com/logilite/dms/querybuildelastic/service/ElasticIndexQueryBuilder.java
+++ com.logilite.dms.querybuildelastic/src/com/logilite/dms/querybuildelastic/service/ElasticIndexQueryBuilder.java
@@ -6,8 +6,11 @@ import java.util.Map.Entry;
 
 import org.compiere.util.Util;
 
+import com.logilite.dms.constant.DMSConstant;
 import com.logilite.dms.factories.IIndexQueryBuilder;
+import com.logilite.dms.model.MDMSAssociation;
 import com.logilite.dms.model.MDMSContent;
+import com.logilite.dms.util.DMSSearchUtils;
 
 /**
  * Index Query Builder for Elastic services
@@ -20,9 +23,6 @@ public class ElasticIndexQueryBuilder implements IIndexQueryBuilder
 	@Override
 	public String buildSearchQueryFromMap(HashMap<String, List<Object>> params)
 	{
-		// TODO: Implement search query
-		System.out.println(params);
-
 		StringBuffer query = new StringBuffer();
 
 		for (Entry<String, List<Object>> row : params.entrySet())
@@ -34,23 +34,23 @@ public class ElasticIndexQueryBuilder implements IIndexQueryBuilder
 			{
 				if (value.get(0) instanceof String && value.get(1) instanceof Boolean && value.get(1) == Boolean.TRUE)
 				{
-					query.append(" , ").append(key + ":" + value.get(0) + "");
+					query.append(" AND \"").append(key + "\" =" + value.get(0));
 				}
 				else if (value.get(0) instanceof String || value.get(1) instanceof String)
 				{
-					query.append(" , \"").append(key + "\":{\"gte\": \"" + value.get(0) + "\" , \"lte\": \"" + value.get(1) + "\" }");
+					query.append(" AND \"").append(key + "\" :*" + value.get(0) + "* TO *" + value.get(1) + "* ");
 				} // Handle condition when two boolean value passed.
 				else if (value.get(0) instanceof Boolean || value.get(1) instanceof Boolean)
 				{
-					query.append(" , ").append(key + ":" + value.get(0) + " OR ").append(key + ":" + value.get(1) + "");
+					query.append(" AND \"").append(key + "\" IN (" + value.get(0) + ", " + value.get(1) + ")");
 				}
 				else if (value.get(1).equals("*"))
 				{
-					query.append(" , ").append(key + ":[\"" + value.get(0) + "\" TO " + value.get(1) + " ]");
+					query.append(" AND \"").append(key + "\" IN (\"" + value.get(0) + "\" , \"" + value.get(1) + "\" )");
 				}
 				else
 				{
-					query.append(" , \"").append(key + "\":{\"gte\": \"" + value.get(0) + "\" , \"lte\": \"" + value.get(1) + "\" }");
+					query.append(" AND \"").append(key + "\" BETWEEN '" + value.get(0) + "' AND '" + value.get(1) + "' ");
 				}
 			}
 			else
@@ -58,50 +58,91 @@ public class ElasticIndexQueryBuilder implements IIndexQueryBuilder
 				if (value.get(0) instanceof String)
 				{
 					if (Util.isEmpty((String) value.get(0), true))
-						query.append(" , ").append(key + ":*");
+						query.append(" AND \"").append(key + "\"='*'");
 					else
-						query.append(" , ").append(key + ":\"" + value.get(0) + "\"");
+						query.append(" AND \"").append(key + "\"='" + value.get(0) + "'");
 				}
 				else
 				{
-					query.append(" , ").append(key + ":" + value.get(0) + "");
+					query.append(" AND \"").append(key + "\"=" + value.get(0) + "");
 				}
 			}
 		}
 
-		System.out.println(query);
 		if (query.length() > 0)
-			query.delete(0, 3);
+			query.delete(0, 5);
 		else
 			query.append("*:*");
 
 		return query.toString();
-	}
+	} // buildSearchQueryFromMap
 
 	@Override
-	public String appendCriteria(String query, int aD_Client_ID, MDMSContent content, int tableID, int recordID, String documentView)
+	public String appendCriteria(String query, int ad_client_ID, MDMSContent content, int tableID, int recordID, String documentView)
 	{
 		// AD_Client_id append for search client wise
 		if (!Util.isEmpty(query))
-			query += " , ";
+			query += " AND ";
 
-		//
-		query += commonSearch(aD_Client_ID, content, tableID, recordID, documentView);
+		query += commonSearch(ad_client_ID, content, tableID, recordID, documentView);
 
 		return query;
-	}
-
-	private String commonSearch(int aD_Client_ID, MDMSContent content, int tableID, int recordID, String documentView)
-	{
-		// TODO
-		return null;
-	}
+	} // appendCriteria
 
 	@Override
-	public String getGenericSearchedContentQuery(String searchText, int aD_Client_ID, MDMSContent content, int tableID, int recordID, String documentView)
+	public String getGenericSearchedContentQuery(String searchText, int ad_client_ID, MDMSContent content, int tableID, int recordID, String documentView)
 	{
 		// TODO
 		return null;
-	}
+	} // getGenericSearchedContentQuery
+
+	private String commonSearch(int AD_Client_ID, MDMSContent content, int tableID, int recordID, String documentView)
+	{
+		StringBuffer query = new StringBuffer();
+		query.append(" \"").append(DMSConstant.AD_CLIENT_ID + "\" =" + AD_Client_ID);
+
+		//
+		StringBuffer hirachicalContent = new StringBuffer("");
+		if (content != null && content.getDMS_Content_ID() > 0)
+		{
+			MDMSAssociation association = MDMSAssociation.getParentAssociationFromContent(content.getDMS_Content_ID(), false, null);
+			if (content.isMounting() && association.getRecord_ID() <= 0)
+			{
+				if (association.getAD_Table_ID() > 0)
+				{
+					hirachicalContent.append(" AND \"AD_Table_ID\"=").append(association.getAD_Table_ID());
+				}
+				else
+				{
+					// Search those content which has any Table ID reference
+					// (ass.getAD_Table_ID() <= 0)
+					hirachicalContent.append("-AD_Table_ID:0");
+					// TODO: need to change Table ID not 0
+				}
+			}
+			else
+			{
+				hirachicalContent.append(" AND \"").append(DMSConstant.DMS_CONTENT_ID).append("\" IN (");
+				DMSSearchUtils.getHierarchicalContent(hirachicalContent, content.getDMS_Content_ID(), AD_Client_ID, tableID, recordID, ", ");
+				hirachicalContent.append(content.getDMS_Content_ID()).append(") ");
+			}
+
+			//
+			query.append("  ").append(hirachicalContent.toString());
+		}
+
+		if (DMSConstant.DOCUMENT_VIEW_DELETED_ONLY_VALUE.equalsIgnoreCase(documentView))
+			query.append(" AND \"").append(DMSConstant.SHOW_INACTIVE).append("\"=true");
+		else if (DMSConstant.DOCUMENT_VIEW_NON_DELETED_VALUE.equalsIgnoreCase(documentView))
+			query.append(" AND \"").append(DMSConstant.SHOW_INACTIVE).append("\"=false");
+
+		if (recordID > 0)
+			query.append(" AND \"").append(DMSConstant.RECORD_ID).append("\"=").append(recordID);
+
+		if (tableID > 0)
+			query.append(" AND \"").append(DMSConstant.AD_TABLE_ID).append("\"=").append(tableID);
+
+		return query.toString();
+	} // commonSearch
 
 }
