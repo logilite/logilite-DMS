538a5697281a97e8ccd5ec5979c54652a20abd63
Sachin Bhimani <sbhimani@logilite.com>
2024-04-08 19:45:35 +0530

    List View - Added Created column, Fix Scrolling issue if page having more content and need to scrolling

--- com.logilite.dms/src/com/logilite/dms/form/WDMSPanel.java
+++ com.logilite.dms/src/com/logilite/dms/form/WDMSPanel.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Stack;
-import java.util.TimeZone;
 import java.util.logging.Level;
 
 import javax.servlet.http.HttpSession;
@@ -773,7 +772,7 @@ public class WDMSPanel extends Panel implements EventListener<Event>, ValueChang
 		mnu_sort_contentType.setAttribute(DMSConstant.ATTRIB_NAME, DMSConstant.ATTRIB_CONTENT_TYPE);
 
 		//
-		DMSConstant.SDF_DATE_FORMAT_WITH_TIME.setTimeZone(TimeZone.getTimeZone("UTC"));
+		// DMSConstant.SDF_DATE_FORMAT_WITH_TIME.setTimeZone(TimeZone.getTimeZone("UTC"));
 		addRootBreadCrumb();
 	}
 
